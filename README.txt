9fa04a8b56825de32af30ba1ceb47ce5b3ba8903
joaquin30 <46060104+joaquin30@users.noreply.github.com>
2023-05-13 13:53:26 -0500

    fix newlines and adding compatibility with windows

--- README.txt
+++ README.txt
@@ -1,31 +1,27 @@
-MapReduce
-==========
+# MapReduce
 
-Requirements
--------------
+Repository: <https://github.com/joaquin30/mapreduce.git>
+
+## Requirements
 
 - Python 3.6+ (for subprocess.run)
 - C++11 compiler (for reduce script)
 
-
-Use
-----
+## Use
 
     python mapreduce.py [map OR reduce] [path_to_script] [source_file] [destination_file]
 
-
-WordCount example (in Linux)
------------------------------
+## WordCount example (in Linux)
 
 - Compile reduce.cpp: `g++ -o reduce reduce.cpp`
 - Run map script: `python mapreduce.py map 'python map.py' example.txt output1.txt`
 - Run reduce script: `python mapreduce.py reduce './reduce' output1.txt output2.txt`
 
+## Notes
 
-Notes
-------
-
-map operation sends a TSV from the source file to the map script of the form:
+- map operation sends a TSV from the source file to the map script of the form:
 
     [number_of_line]\t[line]\n
 
+- There is another reduce script in Python (`reduce.py`) that does the same but is slow
+  due to the start and stop of the Python interpreter
--- mapreduce.py
+++ mapreduce.py
@@ -15,59 +15,69 @@ def warn(msg):
 
 
 def getkv(tsv):
+    """Returns a list of tuples of size 2.
+    If there is no '\t' or more than one per line it gives error
+    """
+
     kvs = []
-    for line in tsv.split('\n'):
-        if len(line) == 0:
-            continue
+    for line in tsv.splitlines():
         kv = tuple(line.split('\t'))
         if len(kv) != 2:
-            die("TSV Format malformed.")
+            die("TSV format malformed")
         kvs.append(kv)
     return kvs
 
 
+def args(script):
+    """Returs process and arguments for subprocess.run
+    """
+
+    if sys.platform == 'win32':
+        return script
+    return shlex.split(script)
+
 def fmap(script, src, dst):
     inp = ""
     try:
-        with open(src, encoding="utf8") as f:
+        with open(src, encoding="utf8", newline="\n") as f:
             ind = 1
             for line in f:
+                # sanitize the input
                 text = line.replace('\t', ' ')
-                text = text.strip('\n')
-                print(text)
+                text = text.splitlines()[0]
                 inp += f"{ind}\t{text}\n"
                 ind += 1
     except:
-        die("Source file doesn't exist or can't be read.")
+        die("Source file doesn't exist or can't be read")
 
     out = None
     try:
-        out = run(shlex.split(script), input=inp.encode(),
+        out = run(args(script), input=inp.encode(),
             capture_output=True).stdout.decode("utf8")
     except:
-        die("The script doesn't exist or failed in execution.")
+        die("The script doesn't exist or failed in execution")
 
     out = getkv(out)
     out.sort()
     try:
-        with open(dst, "w", encoding="utf8") as f:
+        with open(dst, "w", encoding="utf8", newline="\n") as f:
             for k, v in out:
-                f.write(f'{k}\t{v}\n')
+                f.write(f"{k}\t{v}\n")
     except:
-        die("Unable to write to destination file.")
+        die("Unable to write to destination file")
 
 
 def freduce(script, src, dst):
     data = None
     try:
-        with open(src, encoding="utf8") as f:
+        with open(src, encoding="utf8", newline="\n") as f:
             data = f.read()
     except:
-        die("Source file doesn't exist or can't be read.")
+        die("Source file doesn't exist or can't be read")
 
     data = getkv(data)
     if len(data) == 0:
-        warn("Source file empty.")
+        warn("Source file is empty")
         sys.exit()
 
     key = data[0][0]
@@ -78,25 +88,25 @@ def freduce(script, src, dst):
             inp += f"{k}\t{v}\n"
         else:
             try:
-                out += run(shlex.split(script), input=inp.encode(),
+                out += run(args(script), input=inp.encode(),
                             capture_output=True).stdout.decode("utf8")
             except:
-                die("The script doesn't exist or failed in execution.")
+                die("The script doesn't exist, doesn't have permissions or failed in execution")
 
             inp = f"{k}\t{v}\n"
             key = k
 
     try:
-        out += run(shlex.split(script), input=inp.encode(),
+        out += run(args(script), input=inp.encode(),
                     capture_output=True).stdout.decode("utf8")
     except:
-        die("The script doesn't exist or failed in execution.")
+        die("The script doesn't exist or failed in execution")
 
     out = getkv(out)
     out.sort()
 
     try:
-        with open(dst, "w", encoding="utf8") as f:
+        with open(dst, "w", encoding="utf8", newline="\n") as f:
             for k, v in out:
                 f.write(f'{k}\t{v}\n')
     except:
